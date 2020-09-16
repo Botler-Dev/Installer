a717c99a757162dfcfda7c8b09cfc8e54b0b982f
Hunter T <strangeranger01@gmail.com>
2020-09-15 19:53:12 -0700

    build: remove _LINUXPMI variable

--- linuxPMI.sh
+++ linuxPMI.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# TODO: Make comment on purpouse of this variable
+# Keeps track of changes to linuxPMI.sh in combination with the master installer
 export linuxPMI_revision="3"
-export _LINUXPMI="/home/bolter/linuxPMI.sh"
 
 # Checks to see if this script was executed with root privilege
 if ((EUID != 0)); then 
@@ -12,8 +11,9 @@ if ((EUID != 0)); then
 fi
 
 echo "Downloading 'linux-master-installer.sh'..."
-#wget -N https://raw.githubusercontent.com/Botler-Dev/Installer/master/linux-master-installer.sh || {
-wget -N https://raw.githubusercontent.com/Botler-Dev/Installer/dev/linux-master-installer.sh || {
+#wget -N https://raw.githubusercontent.com/Botler-Dev/Installer/release/latest/linux-master-installer.sh || { # Latest release branch
+#wget -N https://raw.githubusercontent.com/Botler-Dev/Installer/master/linux-master-installer.sh || { # Working dev branch
+wget -N https://raw.githubusercontent.com/Botler-Dev/Installer/dev/linux-master-installer.sh || { # Dev branch
     echo "Failed to download 'linux-master-installer.sh'..." >&2
     echo -e "\nExiting..."
     exit 1
